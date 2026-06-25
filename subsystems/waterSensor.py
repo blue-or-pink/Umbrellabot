f2f4e197d42cbde0eb211113ee17a8bef86846f3
blue-or-pink <jane.carol.berry@gmail.com>
2026-06-25 11:19:18 +0900

    wip

--- subsystems/waterSensor.py
+++ subsystems/waterSensor.py
@@ -1,5 +1,20 @@
+from gpiozero import Button
+#
+sensor = Button(17, pull_up=True)
+detected = False
+def waterDetected():
+    detected = True
+def waterNotDetected():
+    detected = False
+
 def init():
     pass
 
+def getValue():
+    return detected
+
 def periodic():
-    pass
+    sensor.when_pressed = waterDetected
+    sensor.when_released = waterNotDetected
+    if detected:
+        print("water detected!")
