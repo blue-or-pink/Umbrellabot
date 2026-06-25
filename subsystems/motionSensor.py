3fbf45d90c1ae94d3f671c68c25d9f2245b6dc45
Lizzard09 <lizzsardinas@gmail.com>
2026-06-25 11:39:54 +0900

    WRK

--- subsystems/motionSensor.py
+++ subsystems/motionSensor.py
@@ -1,5 +1,19 @@
 import subsystems.drivetrain as drivetrain 
 import time 
+import machine
+pir_pin = machine.pin(6, machine.Pin.IN)
+
+def motiondect():
+    try:
+        while True:
+            if pir_pin.value() == 1:
+                print("Motion Detected")
+                time.sleep(1)
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("\Program. stopped.")
+
+
 
 # to tell drivetrain to do something, 
 # use drivetrain.driveCommand = drivetrain.driveForward (etc. - check the functions in drivetrain.py)
@@ -10,4 +24,3 @@ def init():
 def periodic():
     pass
 
-def 
