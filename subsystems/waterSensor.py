324a900b1e4808628c182a2944d2de54eb2ba22f
blue-or-pink <jane.carol.berry@gmail.com>
2026-06-29 10:40:18 +0900

    wip

--- subsystems/waterSensor.py
+++ subsystems/waterSensor.py
@@ -15,7 +15,7 @@ ADC.setup(0x48)
 detected = False
 def detectWater():
     global detected
-    if ADC.read(0) > 1000:
+    if ADC.read(0) > 200:
         detected = True
     else:
         detected = False
