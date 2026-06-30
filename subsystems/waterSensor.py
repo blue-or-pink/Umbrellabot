f806fd25a6caba8f1e5c55590b5ea77c9f5ab632
blue-or-pink <jane.carol.berry@gmail.com>
2026-06-30 09:38:44 +0900

    wip

--- subsystems/waterSensor.py
+++ subsystems/waterSensor.py
@@ -16,7 +16,7 @@ ADC.setup(0x48)
 detected = False
 def detectWater():
     global detected
-    if ADC.read(0) < 25:# 63.5:
+    if ADC.read(0) < 3:# 63.5:
         detected = True
     else:
         detected = False
@@ -38,3 +38,6 @@ def periodic():
 # min = 48
 # max = 79
 # it goes down when in water!!!
+
+# 3 cold and 63.5 room temp
+# lower temp = lower number range
