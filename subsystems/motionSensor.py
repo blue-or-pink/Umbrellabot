7b7449734c97034cd1145b380f0b762e3bf32925
blue-or-pink <jane.carol.berry@gmail.com>
2026-07-01 10:00:58 +0900

    wip

--- subsystems/motionSensor.py
+++ subsystems/motionSensor.py
@@ -1,37 +1,12 @@
 import subsystems.drivetrain as drivetrain 
-import time
+#import time
 #import machine
 #pir_pin = machine.pin(6, machine.Pin.IN)
 from gpiozero import MotionSensor
 
-
 # Initialize the sensor connected to GPIO 4 (Physical Pin 7)
 pir = MotionSensor(6)
 detected = False
-"""
-dir = True // true = left, right = false
-timer = 0
-def motiondect():
-    global detected
-    if pir.motion_detected:
-        detected = True
-        drivetrain.driveCommand = drivetrain.driveForward
-    else:
-        detected = False
-        if timer >= 2000 or timer = 0:
-            if dir:
-                drivetrain.driveCommand = drivetrain.turnLeft
-            else:
-                drivetrain.driveCommand = drivetrain.turnRight
-
-        if timer >= 2000:
-            dir = not dir
-            timer = 0
-        
-        timer += 1
-    
-"""
-
 dir = True #True = left, right = False 
 timer = 0 
 def motiondect():
@@ -53,19 +28,7 @@ def motiondect():
             dir = not dir 
             timer = 0
         timer += 1 
-       
-"""while True:
-    should_B = motiondect()
-    if should_B: 
-        break
-    time.sleep(0.1)
-"""
 
-#recall the function 
-        
-
-def followMotion():
-    pass
 # to tell drivetrain to do something, 
 # use drivetrain.driveCommand = drivetrain.driveForward (etc. - check the functions in drivetrain.py)
 
